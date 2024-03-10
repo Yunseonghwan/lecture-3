fa8a2b205d892574687f30baf913278492ade0e3
Yunseonghwan <sunghwan0208@naver.com>
2024-03-10 14:55:06 +0900

    intersection observer

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 function Card(props) {
-	return (
-		<div className="Card text-center">
-			<img src={props.image}/>
-			<div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
-				{props.children}
-			</div>
-		</div>
-	)
+  const imgRef = useRef(null);
+
+  useEffect(() => {
+    const options = {};
+    const callback = (entries, observer) => {
+      console.log("Entries", entries);
+    };
+    const observer = new IntersectionObserver(callback, options);
+    observer.observe(imgRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div className="Card text-center">
+      <img src={props.image} ref={imgRef} />
+      <div className="p-5 font-semibold text-gray-700 text-xl md:text-lg lg:text-xl keep-all">
+        {props.children}
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
